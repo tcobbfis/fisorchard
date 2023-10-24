334d4f61a3525a8b642c52b7f4d0e99fdc287302
tcobbfis <96081566+tcobbfis@users.noreply.github.com>
2023-10-24 08:07:35 -0500

    added custom user module and update orchard version

---
+++ Fis.Users/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Fis.Users",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Fis.Users",
+    Category = "Fis.Users"
+)]
